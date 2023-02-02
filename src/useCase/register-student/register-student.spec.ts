9f91e98e4121e377905505a74c0aa14fdc14a5c1
Tony <tonytoth1337@gmail.com>
2023-02-02 21:16:59 +0200

    RED: register user usecase execute method

--- src/useCase/register-student/register-student.spec.ts
+++ src/useCase/register-student/register-student.spec.ts
@@ -7,16 +7,21 @@ const feature = loadFeature(path.join(__dirname, './register-student.feature'));
 
 defineFeature(feature, (test) => {
   test('Successfully register a student', ({ given, when, then }) => {
+    let registerStudentUseCase: RegisterStudent;
+
     given('a student is not registered yet', () => {
-        const registerStudentUseCase = new RegisterStudent();
+      registerStudentUseCase = new RegisterStudent();
     });
 
     when('the student is trying to get registered', () => {
-
+      const studentInput = {
+        email: 'tony@email.com',
+        firstName: 'Tony',
+        lastName: 'Toth',
+      };
+      registerStudentUseCase.execute(studentInput);
     });
 
-    then('the student should be successfully registered', () => {
-
-    });
+    then('the student should be successfully registered', () => {});
   });
 });
