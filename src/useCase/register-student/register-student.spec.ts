2555875360821c530bbd0ac12940224b15fbfb2c
Tony <tonytoth1337@gmail.com>
2023-02-03 20:57:57 +0200

    RED

--- src/useCase/register-student/register-student.spec.ts
+++ src/useCase/register-student/register-student.spec.ts
@@ -5,6 +5,12 @@ import { RegisterStudent } from './register-student';
 
 const feature = loadFeature(path.join(__dirname, './register-student.feature'));
 
+interface StudentInput {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
 defineFeature(feature, (test) => {
   test('Successfully register a student', ({ given, when, then }) => {
     let registerStudentUseCase: RegisterStudent;
@@ -15,7 +21,7 @@ defineFeature(feature, (test) => {
     });
 
     when('the student is trying to get registered', async () => {
-      const studentInput = {
+      const studentInput: StudentInput = {
         email: 'tony@email.com',
         firstName: 'Tony',
         lastName: 'Toth',
@@ -31,5 +37,41 @@ defineFeature(feature, (test) => {
         lastName: 'Toth',
       });
     });
+  });
+
+  test('Fails to register a student', ({ given, when, then, and }) => {
+    let registerStudentUseCase: RegisterStudent;
+    let studentInput: Partial<StudentInput>;
+
+    given('a student is not registered yet', () => {
+      registerStudentUseCase = new RegisterStudent();
+    });
+
+    when('the student is trying to register', () => {
+      studentInput = {
+        firstName: 'Hello',
+        lastName: 'World',
+      };
+    });
+
+    and('he types a wrong email', () => {
+      studentInput.email = '';
+    });
+
+    then(
+      'the student should get an error that there was an error while trying to register',
+      async () => {
+        const response = await registerStudentUseCase.execute(studentInput);
+
+        expect(response).toEqual({
+          data: null,
+          errors: [
+            {
+              message: 'Invalid email address',
+            },
+          ],
+        });
+      },
+    );
   });
 });
