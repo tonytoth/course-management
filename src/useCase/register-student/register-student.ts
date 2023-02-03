9915f5fcaca97d3bec4a46a5d72359772d17c207
Tony <tonytoth1337@gmail.com>
2023-02-03 20:59:20 +0200

    GREEN

--- src/useCase/register-student/register-student.ts
+++ src/useCase/register-student/register-student.ts
@@ -1,7 +1,19 @@
+interface StudentInput {
+  email: string;
+  firstName: string;
+  lastName: string;
+}
+
 class RegisterStudent {
   constructor() {}
 
-  async execute(input: {}) {
+  async execute(input: StudentInput) {
+    if (input.email === '') {
+      return {
+        data: null,
+        errors: [{ message: 'Invalid email address' }],
+      };
+    }
     return input;
   }
 }
