92879255c576710d2755c9ca09c590d011abb10c
Tony <tonytoth1337@gmail.com>
2023-02-04 15:09:39 +0200

    RED

--- src/useCase/register-student/domain/student.entity.spec.ts
+++ src/useCase/register-student/domain/student.entity.spec.ts
@@ -12,4 +12,10 @@ describe('student entity', () => {
   it('should have a create method which accepts an object as parameters', () => {
     expect(Student.create({})).toBeInstanceOf(Object);
   });
+
+  it('should return an error object if the email is not valid', () => {
+    expect(Student.create({ email: 'a@a.a' })).toMatchObject({
+      message: 'Email address is invalid',
+    });
+  });
 });
