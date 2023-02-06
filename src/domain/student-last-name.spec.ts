d6094facd3935d48aa98f3f5e241a5b1a0d54c25
Tony <tonytoth1337@gmail.com>
2023-02-06 20:40:14 +0200

    RED

--- src/domain/student-last-name.spec.ts
+++ src/domain/student-last-name.spec.ts
@@ -4,4 +4,8 @@ describe('student last name', () => {
   it('should have a class', () => {
     expect(StudentLastName).toBeDefined();
   });
+
+  it('should have a method called create', () => {
+    expect(StudentLastName.create).toBeDefined();
+  })
 });
