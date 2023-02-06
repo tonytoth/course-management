f57c6d904fd48b2ad65836466eb28aab06b31228
Tony <tonytoth1337@gmail.com>
2023-02-06 20:45:58 +0200

    GREEN

--- src/domain/student-last-name.ts
+++ src/domain/student-last-name.ts
@@ -1,9 +1,12 @@
-import { Result } from "./result";
+import { Result } from './result';
 
 export class StudentLastName {
   private constructor() {}
 
   static create(lastNameInput: string) {
+    if (lastNameInput === '') {
+      return Result.isNotFine('Invalid firstName');
+    }
     return Result.isFine(lastNameInput);
   }
 }
