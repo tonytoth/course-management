96d92e767e3871ec6ae92715b27cd8eea0460450
Tony <tonytoth1337@gmail.com>
2023-03-03 23:50:51 +0200

    init express app + typeorm packages

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
