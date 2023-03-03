7054954be5e3c9caedfd2d4c22db76e1db44fc14
Tony <tonytoth1337@gmail.com>
2023-03-03 23:59:02 +0200

    add infra directory

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-import express from 'express';
-
-const app = express();
-const port = process.env.PORT;
-
-app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+import './shared/infra/http/app';
